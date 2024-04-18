816fb60649a16fd5bc4921306f2b7a7abd47dafb
Romeo Flauta <meoflauta@gmail.com>
2024-04-18 16:39:21 +0800

    Initial Commit

---
+++ EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Romeo Flauta on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
