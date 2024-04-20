f5f664fa3954eb8a7f0ced04bc54bb159e436388
Romeo Flauta <meoflauta@gmail.com>
2024-04-20 18:09:42 +0800

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -6,64 +6,73 @@
 //
 
 import UIKit
+import CoreData
 import EssentialFeed
 import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         /*
-        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
-        let feedLoader = RemoteFeedLoader(url: url, client: client)
-        let imageLoader = RemoteFeedImageDataLoader(client: client)
-        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
-        window?.rootViewController = feedViewController
+         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+         let session = URLSession(configuration: .ephemeral)
+         let client = URLSessionHTTPClient(session: session)
+         let feedLoader = RemoteFeedLoader(url: url, client: client)
+         let imageLoader = RemoteFeedImageDataLoader(client: client)
+         let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
+         window?.rootViewController = feedViewController
          */
         
-        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")! 
-
-        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        
+        let remoteClient = makeRemoteClient()
+        
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-
+        
+        let localStoreURL = NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("feed-store.sqlite")
+        
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+        
         window?.rootViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: remoteFeedLoader,
-            imageLoader: remoteImageLoader)
+            feedLoader: FeedLoaderWithFallbackComposite(
+                primary: FeedLoaderCacheDecorator(
+                    decoratee: remoteFeedLoader,
+                    cache: localFeedLoader),
+                fallback: localFeedLoader),
+            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                primary: localImageLoader,
+                fallback: FeedImageDataLoaderCacheDecorator(
+                    decoratee: remoteImageLoader,
+                    cache: localImageLoader)))
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    
+    private func makeRemoteClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+        case "offline":
+            return AlwaysFailingHTTPClient()
+            
+        default:
+            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        }
     }
+}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
+private class AlwaysFailingHTTPClient: HTTPClient {
+	private class Task: HTTPClientTask {
+		func cancel() {}
+	}
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+	func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+		completion(.failure(NSError(domain: "offline", code: 0)))
+		return Task()
+	}
 }
 
--- EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -13,8 +13,24 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
 
 		app.launch()
         
-        if app.cells.firstMatch.images.matching(identifier: "FeedImage").element.waitForExistence(timeout: 1.0) && app.cells.matching(identifier: "FeedImageCell").element.waitForExistence(timeout: 1.0) {
+        if app.cells.firstMatch.images.matching(identifier: "FeedImageView").element.waitForExistence(timeout: 1.0) && app.cells.matching(identifier: "FeedImageCell").element.waitForExistence(timeout: 1.0) {
             let feedCells = app.cells.matching(identifier: "FeedImageCell")
+            let firstCell = feedCells.firstMatch
+            XCTAssertEqual(feedCells.count, 22)
+            XCTAssertEqual(firstCell.images.count, 22)
+        }
+	}
+    
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+		let onlineApp = XCUIApplication()
+		onlineApp.launch()
+
+		let offlineApp = XCUIApplication()
+		offlineApp.launchArguments = ["-connectivity", "offline"]
+		offlineApp.launch()
+
+        if offlineApp.cells.firstMatch.images.matching(identifier: "FeedImageView").element.waitForExistence(timeout: 1.0) && offlineApp.cells.matching(identifier: "FeedImageCell").element.waitForExistence(timeout: 1.0) {
+            let feedCells = offlineApp.cells.matching(identifier: "FeedImageCell")
             let firstCell = feedCells.firstMatch
             XCTAssertEqual(feedCells.count, 22)
             XCTAssertEqual(firstCell.images.count, 22)
